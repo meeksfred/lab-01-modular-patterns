96cc9f42328e56fb60cced32174b7a4e584fbb6a
meeksfred <maxfred0025@gmail.com>
2016-09-13 13:47:35 -0700

    remove redundant describe() block, add gulp file with tasks

--- lab-max/test/greet-test.js
+++ lab-max/test/greet-test.js
@@ -4,14 +4,12 @@ const helloWorld = require('../lib/greet.js');
 const assert = require('assert');
 
 describe('testing module greet', function(){
-  describe('testing #greet()', function(){
-    it('should return hello max', function(){
-      let answer = helloWorld.greet('max');
-      assert.ok(answer === 'hello max', 'was not hello max');
-    });
-    it('should return a string length greater than 5', function(){
-      let answerLength = helloWorld.greet('bob').length;
-      assert.ok(answerLength > 5, 'not greater than 5');
-    });
+  it('should return hello max', function(){
+    let answer = helloWorld.greet('max');
+    assert.ok(answer === 'hello max', 'was not hello max');
+  });
+  it('should return a string length greater than 5', function(){
+    let answerLength = helloWorld.greet('bob').length;
+    assert.ok(answerLength > 5, 'not greater than 5');
   });
 });
